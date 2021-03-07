23528d7ddc40851be7e54a33ea3ff86177f95533
aditya <adityaraj2019.sahu@gmail.com>
2021-03-07 08:12:11 +0530

    Image Processing based task

---
+++ find_angle/src/threshold.py
@@ -0,0 +1,14 @@
+# include <ros/ros.h>
+# include <opencv2/core.hpp>
+# include <opencv2/imgproc.hpp>
+# include <opencv2/highgui.hpp>
+
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "threshold");
+    ros::NodeHandle n;
+
+    
+}
