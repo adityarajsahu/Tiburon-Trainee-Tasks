215c982b4518ed2b57ddc5751867616611b5cc65
aditya <adityaraj2019.sahu@gmail.com>
2021-03-16 20:31:44 +0530

    Code for edge detection

--- find_angle/src/houghlines.cpp
+++ find_angle/src/houghlines.cpp
@@ -8,6 +8,7 @@
 #include <bits/stdc++.h>
 
 using namespace cv;
+using namespace std;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -15,16 +16,28 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
     frame = cv_bridge::toCvShare(msg, "bgr8")->image;
     cvtColor(frame, colored, COLOR_GRAY2BGR);
-    hough_clone = colored.clone();
+    colored_clone = colored.clone();
 
     vector<Vec2f> lines;
     HoughLines(colored, lines, 1, CV_PI/180, 150, 0, 0);
 
+    float theta1 = lines[0][1], theta2 = lines[1][1];
+    float m1 = tan(theta1), m2 = tan(theta2);
+
+    float tan_of_angle = (m2-m1)/(1+ (m1*m2));
+    float angle = atan(tan_of_angle);
+
+    string disp = "Angle formed : " + to_string(angle);
+    putText(colored, disp, Point(50,50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 200, 200), 4);
+
+    imshow("Angle", colored);
+    waitKey(0);
+    destroyWindow("Angle");
 }
 
 int main(int argc, char **argv)
 {
-    ros::init_node("houghlines");
+    ros::init(argc, argv, "houghlines");
     ros::NodeHandle n;
 
     cv::namedWindow("view2");
